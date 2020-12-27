62f307c8af249a2027abe2864b51228c16100617
fullh <fullh@LAPTOP-RL9DJ704>
2020-12-27 02:29:15 -0500

    Working splitting data phase of mapreduce

--- User/User/main.go
+++ User/User/main.go
@@ -2,26 +2,13 @@ package main
 
 import (
 	Utility "MapReduce/Utility"
-	"log"
-	"os"
 )
 
 func main() {
 
+	number_chunks := 30
 	filename := "C:/Go/src/MapReduce/Book/pg-being_ernest.txt"
 
-	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatalf("cannot open %v", filename)
-	}
-
-	//content, err := ioutil.ReadAll(file)
-	//if err != nil {
-	//	log.Fatalf("cannot read %v", filename)
-	//}
-
-	Utility.Split_Data()
-
-	file.Close()
+	Utility.Split_Data(filename, number_chunks)
 
 }
--- Utility/utility.go
+++ Utility/utility.go
@@ -1,9 +1,56 @@
 package Utility
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
-func Split_Data() {
+func Split_Data(filename string, number_chunks int) {
 
-	fmt.Println("Hello from splitd data")
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatalf("cannot open %v", filename)
+	}
+
+	f, _ := file.Stat()
+
+	//Calculate the size of each chunck
+	chunckSize := int(f.Size()) / number_chunks
+
+	//fmt.Println(chunckSize)
+
+	var pos int64 = 0
+
+	/*
+	  Iterate through file based on number of chuncks and read about chunkSize from the file.
+	  After create new file and write new chunck of data into it.
+
+	*/
+
+	for i := 0; i < number_chunks; i++ {
+
+		pos, _ = file.Seek(pos, 0)
+
+		byteSlice := make([]byte, chunckSize)
+		n, _ := file.Read(byteSlice)
+
+		fmt.Println("Current position is ", pos)
+
+		fmt.Println("Number of bytes read:", n)
+
+		pos = pos + int64(chunckSize)
+
+		f, err := os.Create("C:/Go/src/MapReduce/Chunks/chunk-" + strconv.Itoa(i) + ".txt")
+		defer f.Close()
+
+		if err == nil {
+			fmt.Println("Error opening the file")
+		}
+
+		f.Write(byteSlice)
+
+	}
 
 }
